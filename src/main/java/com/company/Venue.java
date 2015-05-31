2fa6f4cdb03a610d552789c518d51cb38faac0a3
Levon Oganyan <lemarwin42@gmail.com>
2015-06-01 00:49:32 +0300

    Venue get/set and stuff

--- src/main/java/com/company/Venue.java
+++ src/main/java/com/company/Venue.java
@@ -5,4 +5,63 @@ package com.company;
  * Class to hold the data abount a Venue -- a place
  */
 public class Venue {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getWorkTime() {
+        return workTime;
+    }
+
+    public void setWorkTime(String workTime) {
+        this.workTime = workTime;
+    }
+
+    private String address;
+    private String description;
+    private String url;
+    private String workTime;
+
+    public Venue(){
+
+    }
+
+    public Venue(String name, String address, String description, String url, String workTime) {
+        this.name = name;
+        this.address = address;
+        this.description = description;
+        this.url = url;
+        this.workTime = workTime;
+    }
+
 }
